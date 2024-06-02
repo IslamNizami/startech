3f6e624cdf09185f63c6cd6e190802fa1f5fe1e8
IslamNizami <islamnizami0046@gmail.com>
2024-06-02 13:58:24 +0400

    project createt

---
+++ src/main/java/finalproject/startech/StartechApplication.java
@@ -0,0 +1,13 @@
+package finalproject.startech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartechApplication.class, args);
+    }
+
+}
