dabb3bf83d9d1a8e1159ad8b440000aba9f7143b
IslamNizami <islamnizami0046@gmail.com>
2024-06-05 23:29:36 +0400

    Package repo and Model created

---
+++ src/main/java/finalproject/startech/repositories/PackageRepository.java
@@ -0,0 +1,6 @@
+package finalproject.startech.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PackageRepository extends JpaRepository<Package, Long> {
+}
